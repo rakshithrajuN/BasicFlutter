e65781dc0f6c33b30ac1f33e8acc18d737dba47f
rakshithrajuN <rakshithraju2000@gmail.com>
2021-07-13 18:31:09 +0530

    initial changes

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Awesome App",
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Awesome App")),
+      body:Container(child: Center(child: Text("Hi Flutter"))),
+    );
+   
+}
+}
